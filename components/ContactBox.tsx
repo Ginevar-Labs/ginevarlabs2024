ed9088961efbe2f919e03b807b3ee744c2dbd125
Juan Curti <jmfcdev@gmail.com>
2024-01-17 19:01:41 +0100

    Responsiveness of ContactBox

--- components/ContactBox.tsx
+++ components/ContactBox.tsx
@@ -15,6 +15,8 @@ export default function ContactBox(props: ContactBoxProps) {
     const [inputServiceRequested, setInputServiceRequested] = useState(0);
     const [inputDescription, setInputDescription] = useState('');
     const [inputTermsAccepted, setInputTermsAccepted] = useState('true');
+    const [fileList, setFileList] = useState<FileList | null>(null);
+    const [fileNames, setFileNames] = useState(['']);
   
     return (
       <div>
@@ -42,7 +44,7 @@ export default function ContactBox(props: ContactBoxProps) {
                 </h1>
             </div>
 
-            <div className='sm:col-span-1 col-span-1 items-center p-6 px-12 w-full lg:pr-[200px] px-12'>
+            <div className='sm:col-span-1 col-span-1 items-center lg:p-6 p-0 px-12 w-full lg:pr-[200px] px-12'>
                 <div className='flex w-full my-6 mt-10'>
                     <label className='fontedBebas w-[200px]'>Full Name*</label>
                     <div className='w-full border-b-4 border-black drop-shadow-[4px_4px_rgba(255,242,56,0.25)] '>
@@ -90,12 +92,50 @@ export default function ContactBox(props: ContactBoxProps) {
             </div>
 
         </div>
-        <div className='grid sm:grid-cols-2 grid-cols-1 border-dashed border-[2px] border-gray-800 inline-block'>
-            <div className='sm:col-span-1 h-full col-span-1 flex items-center p-20 px-12 border-dashed border-r-[2px] border-gray-800'>
-                <p className='fontedBebas w-[75%]'>If you need us to sign an nda before telling us about your project, SEND IT AS WELL</p>
+        <div className='grid sm:grid-cols-2 grid-cols-1 border-dashed lg:border-[2px] border-[0px] border-gray-800 inline-block'>
+            <div className='sm:col-span-1 h-full col-span-1 flex items-center lg:p-20 p-4 px-12 border-dashed lg:border-r-[2px] border-r-[0px] border-gray-800'>
+                <p className='fontedBebas lg:w-[75%] w-full lg:text-left text-center'>If you need us to sign an nda before telling us about your project, SEND IT AS WELL</p>
             </div>
-            <div className='sm:col-span-1 h-full col-span-1 flex items-center p-20 px-12'>
-                <p>Load file</p>
+            <div className='sm:col-span-1 h-full col-span-1 flex items-center lg:p-20 p-4 px-12'>
+            <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full cursor-pointer">
+                            <div className="flex flex-col items-center justify-center pt-5 pb-6  transition duration-200 opacity-25 hover:opacity-100">
+                                <div className='flex'>
+                                    <svg xmlns="http://www.w3.org/2000/svg" className='' width="22" height="30" viewBox="0 0 53 60" fill="none">
+                                        <g clip-path="url(#clip0_148_716)">
+                                            <path d="M42.6797 9.82031C39.8203 6.96094 35.1797 6.96094 32.3203 9.82031L10.7578 31.3828C5.82422 36.3164 5.82422 44.3086 10.7578 49.2422C15.6914 54.1758 23.6836 54.1758 28.6172 49.2422L46.4297 31.4297C47.707 30.1523 49.793 30.1523 51.0703 31.4297C52.3477 32.707 52.3477 34.793 51.0703 36.0703L33.2578 53.8828C25.7578 61.3828 13.6172 61.3828 6.11719 53.8828C-1.38281 46.3828 -1.38281 34.2422 6.11719 26.7422L27.6797 5.17969C33.1055 -0.246094 41.8945 -0.246094 47.3203 5.17969C52.7461 10.6055 52.7461 19.3945 47.3203 24.8203L26.6953 45.4453C23.3438 48.7969 17.9062 48.7969 14.5547 45.4453C11.2031 42.0938 11.2031 36.6563 14.5547 33.3047L31.4297 16.4297C32.707 15.1523 34.793 15.1523 36.0703 16.4297C37.3477 17.707 37.3477 19.793 36.0703 21.0703L19.1953 37.9453C18.4102 38.7305 18.4102 40.0195 19.1953 40.8047C19.9805 41.5898 21.2695 41.5898 22.0547 40.8047L42.6797 20.1797C45.5391 17.3203 45.5391 12.6797 42.6797 9.82031Z" fill="black"/>
+                                        </g>
+                                        <defs>
+                                            <clipPath id="clip0_148_716">
+                                            <rect width="52.5" height="60" fill="white"/>
+                                            </clipPath>
+                                        </defs>
+                                        </svg>
+                                        <p className="ml-2 text-black fontedBebas"><span className="font-semibold">Attach files</span></p>
+                                </div>
+                                <p className="text-center text-black fontedBebas">Max 5 files. 5mb limit each. Formats allowed doc, docx, pdf, ppt, pptx</p>
+                            </div>
+                            <input onChange={(e) => {
+                              if(e.target.files == undefined) { return; }
+                              for(var x in e.target.files) {
+                                var filesize = ((e.target.files[x].size/1024)/1024).toFixed(4); // MB
+                                if(!isNaN(parseFloat(filesize))) {
+                                  if(parseFloat(filesize) > 5) {
+                                    alert('One or more files are too big. Max size per file is 5 MB');
+                                    return;
+                                  }
+                                }
+                              }
+                              setFileList(e.target.files);
+                              e.preventDefault();
+        
+                              var _itemNames:string[] = []
+                              for(var i=0;i<e.target.files.length;i++){
+                                _itemNames.push(e.target.files[i].name);
+                              }
+                              setFileNames(_itemNames);
+                            }} id="dropzone-file" type="file" className="hidden" multiple/>
+                            <p className='fontedBebas text-center opacity-25'>{fileNames.filter(f => f != "").length > 0 ? 'Files uploaded:' : ''} {fileNames.join(', ')}</p>
+                        </label>
             </div>
         </div>
         <div className='w-full my-6'>
