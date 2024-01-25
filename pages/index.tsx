14e7cbf1016e4686877fd3a7c852dd17511e067f
Juan Curti <jmfcdev@gmail.com>
2024-01-25 20:34:09 +0100

    Start working on Home

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,14 +24,28 @@ export default function Home({ locale }: InferGetServerSidePropsType<typeof getS
     <>
       <Header currentPage='home' locale={locale} />
       <main
-        className={`flex min-h-screen flex-col px-10 py-10 bg-[#1F1D1D] fontedBebas `}
+        className={`flex min-h-screen flex-col bg-[#1F1D1D] fontedBebas `}
       >
-        {/* <HomeHero/>
-        <ServiceOfferingBox locale={locale}/>
-        <TrustedByBox/>
-        <HomeMessageBox message={t('home_message_box_message_1', { ns: 'common'})}/>
-        <EngineeringBlogBox locale={locale} categories={blog.categories} posts={blog.posts} />
-        <ContactBox locale={locale}/> */}
+        <div className='relative w-full lg:h-[460px] h-[220px] flex justify-center items-center border-white border-dashed border-b-[2px]'>
+            <img className='absolute top-0 left-0 w-full h-full object-cover' src='/assets/banners/banner-1.png' alt='Ginevar - Banner'/>
+            <div className='w-full h-full absolute top-0 left-0 bg-black opacity-50'/>
+            <div className='relative grid grid-cols-3 lg:mx-10 mx-2 w-full h-full'>
+              <div className='relative col-span-1 flex content-start justify-start items-top lg:my-10 my-2'>
+                <p className='relative text-left text-white lg:text-[56px] text-[24px] leading-none'>
+                  Crafting <br/> Digital <br/> Excellence
+              </p>
+              </div>
+              <div className='relative w-full col-span-1 flex content-center justify-center items-center'>
+                <img src='/assets/ginevar_logo_dark.png' alt='Ginevar dark logo' className='lg:w-[150px] w-[100px] object-fit' />
+              </div>
+              <div className='relative col-span-1 flex content-end justify-end items-end lg:my-10 my-2'>
+                <p className='relative text-right text-white lg:text-[56px] text-[24px leading-none'>
+                  In the palm <br/>of your hand
+              </p>
+              </div>
+            </div>
+        </div>
+
       </main>
       <Footer currentPage='home' locale={locale}/>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.js"></script>
